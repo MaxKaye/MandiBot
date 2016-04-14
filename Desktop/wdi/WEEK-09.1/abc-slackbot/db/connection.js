b033870fd590c1c29790a86845452452a5ea25b9
Max Kaye <maximillianchina@gmail.com>
2016-04-14 19:01:45 -0400

    transitioning seed data to mongoose

--- Desktop/wdi/WEEK-09.1/abc-slackbot/db/connection.js
+++ Desktop/wdi/WEEK-09.1/abc-slackbot/db/connection.js
@@ -1,4 +1,17 @@
+var mongoose = require("mongoose");
 var seedData = require("./seeds.json");
+
+var CharacterSchema = new mongoose.Schema({
+  symbol: String,
+  pinyin: String,
+  meaning: String,
+  used_as: String,
+  example: String
+});
+
+mongoose.model("Character", CharacterSchema);
+mongoose.connect("mongodb://localhost/abc-slackbot");
+
 module.exports = {
   characters: seedData
 };
