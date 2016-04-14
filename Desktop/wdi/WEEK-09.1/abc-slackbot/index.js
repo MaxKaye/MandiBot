9aa03678f5d1f058636cfc03e3f47764ba4d5541
Max Kaye <maximillianchina@gmail.com>
2016-04-14 12:09:27 -0400

    created views folder, handlebars

--- Desktop/wdi/WEEK-09.1/abc-slackbot/index.js
+++ Desktop/wdi/WEEK-09.1/abc-slackbot/index.js
@@ -1,13 +1,23 @@
 var express = require("express");
+var hbs = require("express-handlebars");
 var frontpage = require("./frontpage");
 
 frontpage();
 
 var app = express();
 
+app.set("view engine", "hbs");
+app.engine(".hbs", hbs({
+  extname: ".hbs",
+  partialDir: "views/",
+  layoutDir: "views/",
+  defaultLayout: "layout-main"
+}));
+
+
 app.get("/", function(req, res){
   console.log("Someone visited my page");
-  res.send("你好");
+  res.render("app-welcome");
 });
 
 app.listen(3001, function(){
