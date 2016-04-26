9d14cbea8075e73ad11f23c0285dc48ad67a5897
Max Kaye <maximillianchina@gmail.com>
2016-04-26 17:46:53 -0400

    hubot scripts are controlled

--- mandibot/scripts/translate.js
+++ mandibot/scripts/translate.js
@@ -1,7 +1,8 @@
 var request = require("request");
 
 module.exports = function(bot){
-  bot.respond(/translate (.*)/, function(message){
+  bot.respond(/boomboom (.*)/, function(message){
+
     request("http://localhost:3001/translate/" + message.match[1], function(err, res, body){
       return message.reply(body);
     });
